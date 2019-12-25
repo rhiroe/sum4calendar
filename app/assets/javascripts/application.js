b17b9240f0fc73b4dcb5aa6495f0543bcb23bb44
Ryosuke Hiroe <ride.poke@gmail.com>
2019-12-25 15:00:53 +0900

    日本語化

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require moment
 //= require fullcalendar
+//= require fullcalendar/lang/ja
 //= require_tree .
 
 document.addEventListener('turbolinks:load', function() {
