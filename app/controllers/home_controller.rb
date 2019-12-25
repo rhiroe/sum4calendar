f98f0d95bd356ce3c1bf413b31ae4f2abae89610
Ryosuke Hiroe <ride.poke@gmail.com>
2019-12-25 14:44:08 +0900

    高速化

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,16 @@ class HomeController < ApplicationController
   def index; end
 
   def calendar
+    @sum4dates = data.pluck(:start)
     render json: data
   end
 
   private
 
   def data
-    stert_day, end_day = params.permit(:start, :end).values
+    stert_day, end_day = params.permit(:start, :end).values.map(&:to_date)
     (stert_day..end_day).map do |day|
-      num = day.split('-').flat_map { _1.chars.map(&:to_i) }.sum
+      num = day.to_s.split('-').flat_map { _1.chars.map(&:to_i) }.sum
       while num >= 10
         num = num.to_s.chars.map(&:to_i).sum
       end
